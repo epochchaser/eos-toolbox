960f00a3c008dd5794e15d57d0ba675b732e4e46
kai.1001 <kai.1001@kakaocorp.com>
2018-07-12 13:16:50 +0900

    Login with store

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,103 +1,106 @@
-import React from "react"
+import React, { Component } from "react"
 import "../styles/Header.scss"
 import LoginView from "../views/LoginView"
 
-const Header = props => {
-  return (
-    <nav className="navbar header-navbar pcoded-header">
-      <div className="navbar-wrapper">
-        <div className="navbar-logo">
-          <a className="mobile-menu" id="mobile-collapse" href="#!">
-            <i className="ti-menu" />
-          </a>
-          <h5>EOS TOOLBOX</h5>
-        </div>
+class Header extends Component {
 
-        <div className="navbar-container container-fluid">
-          <ul className="nav-left">
-            <li>
-              <div className="sidebar_toggle">
-                <a href="javascript:void(0)">
-                  <i className="ti-menu" />
+  render() {
+    return (
+      <nav className="navbar header-navbar pcoded-header">
+        <div className="navbar-wrapper">
+          <div className="navbar-logo">
+            <a className="mobile-menu" id="mobile-collapse" href="#!">
+              <i className="ti-menu" />
+            </a>
+            <h5>EOS TOOLBOX</h5>
+          </div>
+
+          <div className="navbar-container container-fluid">
+            <ul className="nav-left">
+              <li>
+                <div className="sidebar_toggle">
+                  <a href="javascript:void(0)">
+                    <i className="ti-menu" />
+                  </a>
+                </div>
+              </li>
+            </ul>
+            <ul className="nav-right">
+              <li className="header-notification">
+                <a href="#!">
+                  <i className="ti-bell" />
+                  <span className="badge bg-c-pink" />
                 </a>
-              </div>
-            </li>
-          </ul>
-          <ul className="nav-right">
-            <li className="header-notification">
-              <a href="#!">
-                <i className="ti-bell" />
-                <span className="badge bg-c-pink" />
-              </a>
-              <ul className="show-notification">
-                <li>
-                  <h6>Notifications</h6>
-                  <label className="label label-danger">New</label>
-                </li>
-                <li>
-                  <div className="media">
-                    <img
-                      className="d-flex align-self-center img-radius"
-                      src="../files/assets/images/avatar-2.jpg"
-                      alt="Generic placeholder image"
-                    />
-                    <div className="media-body">
-                      <h5 className="notification-user">John Doe</h5>
-                      <p className="notification-msg">
-                        Lorem ipsum dolor sit amet, consectetuer elit.
-                      </p>
-                      <span className="notification-time">30 minutes ago</span>
+                <ul className="show-notification">
+                  <li>
+                    <h6>Notifications</h6>
+                    <label className="label label-danger">New</label>
+                  </li>
+                  <li>
+                    <div className="media">
+                      <img
+                        className="d-flex align-self-center img-radius"
+                        src="../files/assets/images/avatar-2.jpg"
+                        alt="Generic placeholder image"
+                      />
+                      <div className="media-body">
+                        <h5 className="notification-user">John Doe</h5>
+                        <p className="notification-msg">
+                          Lorem ipsum dolor sit amet, consectetuer elit.
+                        </p>
+                        <span className="notification-time">30 minutes ago</span>
+                      </div>
                     </div>
-                  </div>
-                </li>
-                <li>
-                  <div className="media">
-                    <img
-                      className="d-flex align-self-center img-radius"
-                      src="../files/assets/images/avatar-4.jpg"
-                      alt="Generic placeholder image"
-                    />
-                    <div className="media-body">
-                      <h5 className="notification-user">Joseph William</h5>
-                      <p className="notification-msg">
-                        Lorem ipsum dolor sit amet, consectetuer elit.
-                      </p>
-                      <span className="notification-time">30 minutes ago</span>
+                  </li>
+                  <li>
+                    <div className="media">
+                      <img
+                        className="d-flex align-self-center img-radius"
+                        src="../files/assets/images/avatar-4.jpg"
+                        alt="Generic placeholder image"
+                      />
+                      <div className="media-body">
+                        <h5 className="notification-user">Joseph William</h5>
+                        <p className="notification-msg">
+                          Lorem ipsum dolor sit amet, consectetuer elit.
+                        </p>
+                        <span className="notification-time">30 minutes ago</span>
+                      </div>
                     </div>
-                  </div>
-                </li>
-                <li>
-                  <div className="media">
-                    <img
-                      className="d-flex align-self-center img-radius"
-                      src="../files/assets/images/avatar-3.jpg"
-                      alt="Generic placeholder image"
-                    />
-                    <div className="media-body">
-                      <h5 className="notification-user">Sara Soudein</h5>
-                      <p className="notification-msg">
-                        Lorem ipsum dolor sit amet, consectetuer elit.
-                      </p>
-                      <span className="notification-time">30 minutes ago</span>
+                  </li>
+                  <li>
+                    <div className="media">
+                      <img
+                        className="d-flex align-self-center img-radius"
+                        src="../files/assets/images/avatar-3.jpg"
+                        alt="Generic placeholder image"
+                      />
+                      <div className="media-body">
+                        <h5 className="notification-user">Sara Soudein</h5>
+                        <p className="notification-msg">
+                          Lorem ipsum dolor sit amet, consectetuer elit.
+                        </p>
+                        <span className="notification-time">30 minutes ago</span>
+                      </div>
                     </div>
-                  </div>
-                </li>
-              </ul>
-            </li>
-            <li className="">
-              <a href="#!" className="displayChatbox">
-                <i className="ti-comments" />
-                <span className="badge bg-c-green" />
-              </a>
-            </li>
-            <li className="user-profile header-notification">
-              <LoginView />
-            </li>
-          </ul>
+                  </li>
+                </ul>
+              </li>
+              <li className="">
+                <a href="#!" className="displayChatbox">
+                  <i className="ti-comments" />
+                  <span className="badge bg-c-green" />
+                </a>
+              </li>
+              <li className="user-profile header-notification">
+                <LoginView />
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
-  )
+      </nav>
+    )
+  }
 }
 
 export default Header
