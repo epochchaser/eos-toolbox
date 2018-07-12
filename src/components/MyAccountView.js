08a30f9ae58988eaa446649e189bad217987c56a
chaser <epochchaser@gmail.com>
2018-07-13 01:08:48 +0900

    Card design

--- src/components/MyAccountView.js
+++ src/components/MyAccountView.js
@@ -13,14 +13,27 @@ class MyAccountView extends Component {
   render() {
     return (
       <Fragment>
-        {
-            this.accountStore.isLogin &&
-            <div>{this.accountStore.account.name}</div>
-        }
-        {
-            this.accountStore.accountInfo &&
-            <div>{this.accountStore.eosBalance} EOS</div>
-        }
+        <div class="col-md-6 col-xl-3">
+          <div class="card bg-c-pink order-card">
+            <div class="card-block">
+              <h6 class="m-b-20">Balance</h6>
+              <h2 class="text-right">
+                <i class="ti-wallet f-left" />
+                {this.accountStore.accountInfo && (
+                  <span>{`${this.accountStore.eosBalance} EOS`}</span>
+                )}
+              </h2>
+              <p class="m-b-0">
+                Account
+                {this.accountStore.isLogin && (
+                  <span class="f-right">{this.accountStore.account.name}</span>
+                )}
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* {this.accountStore.isLogin && <div>{this.accountStore.account.name}</div>}
+        {this.accountStore.accountInfo && <div>{this.accountStore.eosBalance} EOS</div>} */}
       </Fragment>
     )
   }
