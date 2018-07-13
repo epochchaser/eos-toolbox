c1b17e3c5ef4605307831a8ad317c4afabdf99e9
lky1001 <lky1001@gmail.com>
2018-07-14 02:12:53 +0900

    resource usage

--- src/components/ResourceView.js
+++ src/components/ResourceView.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react'
 import { inject, observer } from 'mobx-react'
+import NumberFormat from 'react-number-format'
 
 @inject('accountStore')
 @observer
@@ -11,6 +12,22 @@ class ResourceView extends Component {
   }
 
   render() {
+    let ramUsagePercent =
+      parseInt(
+        (this.accountStore.accountInfo.ram_usage / this.accountStore.accountInfo.ram_quota) * 100
+      ) + '%'
+    let cpuUsagePercent =
+      parseInt(
+        (this.accountStore.accountInfo.cpu_limit.used /
+          this.accountStore.accountInfo.cpu_limit.max) *
+          100
+      ) + '%'
+    let netUsagePercent =
+      parseInt(
+        (this.accountStore.accountInfo.net_limit.used /
+          this.accountStore.accountInfo.net_limit.max) *
+          100
+      ) + '%'
     return (
       <Fragment>
         <div className="main-body">
@@ -21,7 +38,7 @@ class ResourceView extends Component {
                   <div className="card summery-card">
                     <div className="card-header">
                       <div className="card-header-left ">
-                        <h5>Resources</h5>
+                        <h5>Resource Usage</h5>
                       </div>
                       <div className="card-header-right">
                         <ul className="list-unstyled card-option">
@@ -43,7 +60,83 @@ class ResourceView extends Component {
                         </ul>
                       </div>
                     </div>
-                    <div className="card-block" />
+                    <div className="row">
+                      <div className="col-md-6 col-lg-4">
+                        <div>
+                          <div className="card-block user-radial-card">
+                            <div
+                              data-label="RAM"
+                              className="radial-bar radial-bar-90 radial-bar-lg radial-bar-danger"
+                            />
+                            <span className="f-26 text-c-pink" style={{ margin: '5px;' }}>
+                              <NumberFormat
+                                value={this.accountStore.accountInfo.ram_usage}
+                                displayType={'text'}
+                                thousandSeparator={true}
+                              />({ramUsagePercent})
+                            </span>
+                            <p>
+                              From{' '}
+                              <NumberFormat
+                                value={this.accountStore.accountInfo.ram_quota}
+                                displayType={'text'}
+                                thousandSeparator={true}
+                              />
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="col-md-6 col-lg-4">
+                        <div>
+                          <div className="card-block user-radial-card">
+                            <div
+                              data-label="CPU"
+                              className="radial-bar radial-bar-90 radial-bar-lg radial-bar-danger"
+                            />
+                            <span className="f-26 text-c-pink">
+                              <NumberFormat
+                                value={this.accountStore.accountInfo.cpu_limit.used}
+                                displayType={'text'}
+                                thousandSeparator={true}
+                              />({cpuUsagePercent})
+                            </span>
+                            <p>
+                              From{' '}
+                              <NumberFormat
+                                value={this.accountStore.accountInfo.cpu_limit.max}
+                                displayType={'text'}
+                                thousandSeparator={true}
+                              />
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="col-md-6 col-lg-4">
+                        <div>
+                          <div className="card-block user-radial-card">
+                            <div
+                              data-label="NET"
+                              className="radial-bar radial-bar-90 radial-bar-lg radial-bar-danger"
+                            />
+                            <span className="f-26 text-c-pink">
+                              <NumberFormat
+                                value={this.accountStore.accountInfo.net_limit.used}
+                                displayType={'text'}
+                                thousandSeparator={true}
+                              />({netUsagePercent})
+                            </span>
+                            <p>
+                              From{' '}
+                              <NumberFormat
+                                value={this.accountStore.accountInfo.net_limit.max}
+                                displayType={'text'}
+                                thousandSeparator={true}
+                              />
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
