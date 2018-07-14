3322ed9646af1f28daf3c6837fbc5bc5070df26e
chaser <epochchaser@gmail.com>
2018-07-14 13:20:37 +0900

    voting layout

--- src/pages/Voting.js
+++ src/pages/Voting.js
@@ -17,7 +17,65 @@ class Voting extends Component {
   }
 
   render() {
-    return <div />
+    return (
+      <div class="card">
+        <div class="card-header">
+          <h5>Producers</h5>
+          <span>
+            vote to <code>BLOCK PRODUCERS</code> that you want
+          </span>
+          <div class="card-header-right">
+            <ul class="list-unstyled card-option">
+              <li>
+                <i class="fa fa-chevron-left" />
+              </li>
+              <li>
+                <i class="fa fa-window-maximize full-card" />
+              </li>
+              <li>
+                <i class="fa fa-minus minimize-card" />
+              </li>
+              <li>
+                <i class="fa fa-refresh reload-card" />
+              </li>
+              <li>
+                <i class="fa fa-times close-card" />
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="card-block table-border-style">
+          <div class="table-responsive">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Producer</th>
+                  <th>Total votes</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>starteosiobp</td>
+                  <td>2.73%</td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>eos42freedom</td>
+                  <td>2.66%</td>
+                </tr>
+                <tr>
+                  <th scope="row">3</th>
+                  <td>eoscanadacom</td>
+                  <td>2.65%</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
