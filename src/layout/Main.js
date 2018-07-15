3637473debe4215208aab3d190213c990cd62eb1
lky1001 <lky1001@gmail.com>
2018-07-15 23:37:20 +0900

    scatter init

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Routes from '../Routes'
+import { inject, observer } from 'mobx-react'
 
-const Main = props => {
-  return (
-    <main>
-      <Routes />
-    </main>
-  )
+@inject('commonStore')
+@observer
+class Main extends Component {
+  constructor(props) {
+    super(props)
+    let { commonStore } = this.props
+    this.commonStore = commonStore
+  }
+
+  render() {
+    if (!this.commonStore._initilizedScatter) {
+      return <main />
+    }
+    return (
+      <main>
+        <Routes />
+      </main>
+    )
+  }
 }
 
 export default Main
--- src/pages/FindAccount.js
+++ src/pages/FindAccount.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 
 class FindAccount extends Component {
+  componentDidMount() {
+    console.log(window.scatter)
+  }
+
   render() {
     return <div>반갑구나 find account</div>
   }
