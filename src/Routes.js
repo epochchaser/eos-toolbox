7ed2e02de3710cdda443dab931facf4a8abcb5a6
chaser <epochchaser@gmail.com>
2018-07-17 13:59:50 +0900

    routing

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Route, Switch } from 'react-router-dom'
-import { Home, MyAccount, FindAccount, Voting, BlockExplorer } from './pages'
+import { Home, MyAccount, FindAccount, Voting, BlockExplorer, Undelegate } from './pages'
 import { inject, observer } from '../node_modules/mobx-react'
 
 @inject('commonStore')
@@ -14,9 +14,9 @@ class Routes extends Component {
         <Route exact path="/" component={Home} />
         {_initilizedEos && <Route exact path="/findaccount" component={FindAccount} />}
         <Route exact path="/account" component={MyAccount} />
-        <Route exact path="/findaccount" component={FindAccount} />
-        <Route exact path="/voting" component={Voting} />
+        <Route exact path="/account/undelegate" component={Undelegate} />
         <Route exact path="/blockexplorer" component={BlockExplorer} />
+        <Route exact path="/voting/dovoting" component={Voting} />
       </Switch>
     )
   }
--- src/components/account/UndelegateView.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-class UndelegateView extends Component {
-  render() {
-    return <div />
-  }
-}
-
-export default UndelegateView
