d95751fddd1c6dacc987ac154199c3fe3ed3218d
kai.1001 <kai.1001@kakaocorp.com>
2018-07-18 11:19:12 +0900

    Change account design

--- src/components/blockexplorer/SearchResult.js
+++ src/components/blockexplorer/SearchResult.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { inject, observer } from 'mobx-react'
 import { FormattedMessage } from 'react-intl'
 import AccountView from './AccountView'
+import TransactionView from './TransactionView'
 
 @inject('explorerStore')
 @observer
@@ -14,38 +15,46 @@ class SearchResult extends Component {
 
   render() {
     return (
-      <div className="col-sm-12 search2 search-result">
-        <div className="card card-main">
-          <div className="card-block">
-            <div className="row">
-              <div className="col-sm-12">
-                <div className="search-content">
-                  <div className="card-block m-t-40 col-sm-12">
-                    {!this.explorerStore.isLoading &&
-                      !this.explorerStore.account &&
-                      !this.explorerStore.accounts &&
-                      !this.explorerStore.transaction && (
-                        <p className="card-text text-muted text-center">
-                          <FormattedMessage id="No Seasrch Results" />
-                        </p>
-                      )}
-                    {this.explorerStore.isLoading && (
-                      <div className="preloader3 loader-block m-t-20" style={{ height: '9px' }}>
-                        <div className="circ1" />
-                        <div className="circ2" />
-                        <div className="circ3" />
-                        <div className="circ4" />
+      <Fragment>
+        {(this.explorerStore.isLoading ||
+          (!this.explorerStore.isLoading &&
+            !this.explorerStore.account &&
+            !this.explorerStore.accounts &&
+            !this.explorerStore.transaction)) && (
+            <div className="col-sm-12 search2 search-result">
+              <div className="card card-main">
+                <div className="card-block">
+                  <div className="row">
+                    <div className="col-sm-12">
+                      <div className="search-content">
+                        <div className="card-block m-t-40 col-sm-12">
+                          {!this.explorerStore.isLoading &&
+                          !this.explorerStore.account &&
+                          !this.explorerStore.accounts &&
+                          !this.explorerStore.transaction && (
+                              <p className="card-text text-muted text-center">
+                                <FormattedMessage id="No Seasrch Results" />
+                              </p>
+                            )}
+                          {this.explorerStore.isLoading && (
+                            <div className="preloader3 loader-block m-t-20" style={{ height: '9px' }}>
+                              <div className="circ1" />
+                              <div className="circ2" />
+                              <div className="circ3" />
+                              <div className="circ4" />
+                            </div>
+                          )}
+                        </div>
                       </div>
-                    )}
-
-                    {!this.explorerStore.isLoading && this.explorerStore.account && <AccountView />}
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-      </div>
+          )}
+        {!this.explorerStore.isLoading && this.explorerStore.account && <AccountView />}
+        {!this.explorerStore.isLoading && this.explorerStore.transaction && <TransactionView />}
+      </Fragment>
     )
   }
 }
--- src/components/blockexplorer/TransactionView.js
+++ src/components/blockexplorer/TransactionView.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { inject, observer } from 'mobx-react'
 import NumberFormat from 'react-number-format'
 import { FormattedMessage } from 'react-intl'
@@ -10,6 +10,65 @@ class TransactionView extends Component {
     super(props)
     let { explorerStore } = this.props
     this.explorerStore = explorerStore
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div class="col-sm-12">
+          <div class="card">
+            <div class="card-header">
+              <div class="card-header-left">
+                <h5>Revenue report</h5>
+              </div>
+              <div class="card-header-right">
+                <ul class="list-unstyled card-option">
+                  <li>
+                    <i class="icofont icofont-simple-left " />
+                  </li>
+                  <li>
+                    <i class="icofont icofont-maximize full-card" />
+                  </li>
+                  <li>
+                    <i class="icofont icofont-minus minimize-card" />
+                  </li>
+                  <li>
+                    <i class="icofont icofont-refresh reload-card" />
+                  </li>
+                  <li>
+                    <i class="icofont icofont-error close-card" />
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="card-block p-0">
+              <div class="card-block-big revenue-report">
+                <div class="row">
+                  <div class="col-lg-3">
+                    <h3 class="f-w-400">$5,763</h3>
+                  </div>
+                  <div class="col-lg-3">
+                    <canvas id="revenue-report" height="150" />
+                  </div>
+                  <div class="col-lg-4">
+                    <div class="revenue-card p-b-10">
+                      <span class="bg-c-blue" />
+                      <h6 class="text-muted d-inline-block m-l-20">This month</h6>
+                      <h6 class="d-inline-block m-l-20 f-w-600">$57,423</h6>
+                    </div>
+                    <div class="revenue-card b-t-default  p-t-10">
+                      <span class="bg-c-green m-t-10" />
+                      <h6 class="text-muted d-inline-block m-l-20">This month</h6>
+                      <h6 class="d-inline-block m-l-20 f-w-600">$48,689</h6>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Fragment>
+    )
   }
 }
 
