0314b098183c5eb1d2a96a8d44cb6745a6e9c4dc
kylee <kylee@devband.com>
2018-07-23 00:43:17 +0900

    my tokens

--- src/components/blockexplorer/AccountView.js
+++ src/components/blockexplorer/AccountView.js
@@ -5,6 +5,7 @@ import NumberFormat from 'react-number-format'
 import { FormattedMessage } from 'react-intl'
 import { format } from 'date-fns'
 import UsageResourceView from '../UsageResourceView'
+import TokenView from '../account/TokenView'
 
 @inject('explorerStore')
 @observer
@@ -183,29 +184,253 @@ class AccountView extends Component {
           <UsageResourceView resource={netResource} />
         </div>
         <div className="col-sm-12">
-          <div className="row m-l-10 m-b-20">
-            <div className="col-sm-12 p-0">
-              <h5><FormattedMessage id="Tokens" /></h5>
+          <TokenView tokens={this.explorerStore.tokens} />
+        </div>
+        <div class="col-sm-12">
+          <div class="card tabs-card">
+            <div class="card-block p-0">
+              <ul class="nav nav-tabs md-tabs" role="tablist">
+                <li class="nav-item">
+                  <a class="nav-link active" data-toggle="tab" href="#home3" role="tab">
+                    <i class="fa fa-home" />Home
+                  </a>
+                  <div class="slide bg-c-blue" />
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" data-toggle="tab" href="#profile3" role="tab">
+                    <i class="fa fa-key" />Security
+                  </a>
+                  <div class="slide bg-c-green" />
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" data-toggle="tab" href="#messages3" role="tab">
+                    <i class="fa fa-play-circle" />Entertainment
+                  </a>
+                  <div class="slide bg-c-pink" />
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" data-toggle="tab" href="#settings3" role="tab">
+                    <i class="fa fa-database" />Big Data
+                  </a>
+                  <div class="slide bg-c-yellow" />
+                </li>
+              </ul>
+
+              <div class="tab-content card-block">
+                <div class="tab-pane active" id="home3" role="tabpanel">
+                  <div class="table-responsive">
+                    <table class="table">
+                      <tr>
+                        <th>Image</th>
+                        <th>Product Code</th>
+                        <th>Customer</th>
+                        <th>Purchased On</th>
+                        <th>Status</th>
+                        <th>Transaction ID</th>
+                      </tr>
+                      <tr>
+                        <td>
+                          <img
+                            src="../files/assets/images/product/prod2.jpg"
+                            alt="prod img"
+                            class="img-fluid"
+                          />
+                        </td>
+                        <td>PNG002344</td>
+                        <td>John Deo</td>
+                        <td>05-01-2017</td>
+                        <td>
+                          <span class="label label-danger">Faild</span>
+                        </td>
+                        <td>#7234486</td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <img
+                            src="../files/assets/images/product/prod3.jpg"
+                            alt="prod img"
+                            class="img-fluid"
+                          />
+                        </td>
+                        <td>PNG002653</td>
+                        <td>Eugine Turner</td>
+                        <td>04-01-2017</td>
+                        <td>
+                          <span class="label label-success">Delivered</span>
+                        </td>
+                        <td>#7234417</td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <img
+                            src="../files/assets/images/product/prod4.jpg"
+                            alt="prod img"
+                            class="img-fluid"
+                          />
+                        </td>
+                        <td>PNG002156</td>
+                        <td>Jacqueline Howell</td>
+                        <td>03-01-2017</td>
+                        <td>
+                          <span class="label label-warning">Pending</span>
+                        </td>
+                        <td>#7234454</td>
+                      </tr>
+                    </table>
+                  </div>
+                  <div class="text-center">
+                    <button class="btn btn-outline-primary btn-round btn-sm">Load More</button>
+                  </div>
+                </div>
+                <div class="tab-pane" id="profile3" role="tabpanel">
+                  <div class="table-responsive">
+                    <table class="table">
+                      <tr>
+                        <th>Image</th>
+                        <th>Product Code</th>
+                        <th>Customer</th>
+                        <th>Purchased On</th>
+                        <th>Status</th>
+                        <th>Transaction ID</th>
+                      </tr>
+                      <tr>
+                        <td>
+                          <img
+                            src="../files/assets/images/product/prod3.jpg"
+                            alt="prod img"
+                            class="img-fluid"
+                          />
+                        </td>
+                        <td>PNG002653</td>
+                        <td>Eugine Turner</td>
+                        <td>04-01-2017</td>
+                        <td>
+                          <span class="label label-success">Delivered</span>
+                        </td>
+                        <td>#7234417</td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <img
+                            src="../files/assets/images/product/prod4.jpg"
+                            alt="prod img"
+                            class="img-fluid"
+                          />
+                        </td>
+                        <td>PNG002156</td>
+                        <td>Jacqueline Howell</td>
+                        <td>03-01-2017</td>
+                        <td>
+                          <span class="label label-warning">Pending</span>
+                        </td>
+                        <td>#7234454</td>
+                      </tr>
+                    </table>
+                  </div>
+                  <div class="text-center">
+                    <button class="btn btn-outline-primary btn-round btn-sm">Load More</button>
+                  </div>
+                </div>
+                <div class="tab-pane" id="messages3" role="tabpanel">
+                  <div class="table-responsive">
+                    <table class="table">
+                      <tr>
+                        <th>Image</th>
+                        <th>Product Code</th>
+                        <th>Customer</th>
+                        <th>Purchased On</th>
+                        <th>Status</th>
+                        <th>Transaction ID</th>
+                      </tr>
+                      <tr>
+                        <td>
+                          <img
+                            src="../files/assets/images/product/prod1.jpg"
+                            alt="prod img"
+                            class="img-fluid"
+                          />
+                        </td>
+                        <td>PNG002413</td>
+                        <td>Jane Elliott</td>
+                        <td>06-01-2017</td>
+                        <td>
+                          <span class="label label-primary">Shipping</span>
+                        </td>
+                        <td>#7234421</td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <img
+                            src="../files/assets/images/product/prod4.jpg"
+                            alt="prod img"
+                            class="img-fluid"
+                          />
+                        </td>
+                        <td>PNG002156</td>
+                        <td>Jacqueline Howell</td>
+                        <td>03-01-2017</td>
+                        <td>
+                          <span class="label label-warning">Pending</span>
+                        </td>
+                        <td>#7234454</td>
+                      </tr>
+                    </table>
+                  </div>
+                  <div class="text-center">
+                    <button class="btn btn-outline-primary btn-round btn-sm">Load More</button>
+                  </div>
+                </div>
+                <div class="tab-pane" id="settings3" role="tabpanel">
+                  <div class="table-responsive">
+                    <table class="table">
+                      <tr>
+                        <th>Image</th>
+                        <th>Product Code</th>
+                        <th>Customer</th>
+                        <th>Purchased On</th>
+                        <th>Status</th>
+                        <th>Transaction ID</th>
+                      </tr>
+                      <tr>
+                        <td>
+                          <img
+                            src="../files/assets/images/product/prod1.jpg"
+                            alt="prod img"
+                            class="img-fluid"
+                          />
+                        </td>
+                        <td>PNG002413</td>
+                        <td>Jane Elliott</td>
+                        <td>06-01-2017</td>
+                        <td>
+                          <span class="label label-primary">Shipping</span>
+                        </td>
+                        <td>#7234421</td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <img
+                            src="../files/assets/images/product/prod2.jpg"
+                            alt="prod img"
+                            class="img-fluid"
+                          />
+                        </td>
+                        <td>PNG002344</td>
+                        <td>John Deo</td>
+                        <td>05-01-2017</td>
+                        <td>
+                          <span class="label label-danger">Faild</span>
+                        </td>
+                        <td>#7234486</td>
+                      </tr>
+                    </table>
+                  </div>
+                  <div class="text-center">
+                    <button class="btn btn-outline-primary btn-round btn-sm">Load More</button>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="row">
-            {this.explorerStore.tokens &&
-              this.explorerStore.tokens.length > 0 && (
-                <Fragment>
-                  {this.explorerStore.tokens.map((token, index) => (
-                    <div className="col-md-4 col-sm-6" key={index}>
-                      <div className="card user-card">
-                        <div className="card-header">
-                          <h5>{token.split(' ')[1]}</h5>
-                        </div>
-                        <div className="card-block">
-                          <h4>{token.split(' ')[0]}</h4>
-                        </div>
-                      </div>
-                    </div>
-                  ))}
-                </Fragment>
-              )}
           </div>
         </div>
         <div className="col-sm-12">
