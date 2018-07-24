4ed9db22afe613839bdfd19578cbb7d3a25defcb
kai.1001 <kai.1001@kakaocorp.com>
2018-07-24 15:53:04 +0900

    class to className

--- src/components/constitution/EosConstitutionView.js
+++ src/components/constitution/EosConstitutionView.js
@@ -6,129 +6,129 @@ class EosConstitutionView extends Component {
     return (
       <div className="col-sm-12">
         <div className="card">
-          <div class="card-block">
-            <h5 class="m-b-20">
+          <div className="card-block">
+            <h5 className="m-b-20">
               <FormattedMessage id="Article I - No Initiation of Violence" />
             </h5>
-            <p class="text-muted m-b-20">
+            <p className="text-muted m-b-20">
               <FormattedMessage id="Members shall not initiate violence or the threat of violence against another Member." />
             </p>
 
-            <h5 class="m-b-20 b-t-default p-t-20">
+            <h5 className="m-b-20 b-t-default p-t-20">
               <FormattedMessage id="Article II - No Perjury" />
             </h5>
-            <p class="text-muted m-b-20">
+            <p className="text-muted m-b-20">
               <FormattedMessage id="Members shall be liable for losses caused by false or misleading attestations and shall forfeit any profit gained thereby." />
             </p>
 
-            <h5 class="m-b-20 b-t-default p-t-20">
+            <h5 className="m-b-20 b-t-default p-t-20">
               <FormattedMessage id="Article III - Rights" />
             </h5>
-            <p class="text-muted m-b-20">
+            <p className="text-muted m-b-20">
               <FormattedMessage id="The Members grant the right of contract and of private property to each other, therefore no property shall change hands except with the consent of the owner, by a valid Arbitrator’s order, or via community referendum. This Constitution creates no positive rights for or between any Members." />
             </p>
 
-            <h5 class="m-b-20 b-t-default p-t-20">
+            <h5 className="m-b-20 b-t-default p-t-20">
               <FormattedMessage id="Article IV - No Vote Buying" />
             </h5>
-            <p class="text-muted m-b-20">
+            <p className="text-muted m-b-20">
               <FormattedMessage id="No Member shall offer nor accept anything of value in exchange for a vote of any type, nor shall any Member unduly influence the vote of another." />
             </p>
 
-            <h5 class="m-b-20 b-t-default p-t-20">
+            <h5 className="m-b-20 b-t-default p-t-20">
               <FormattedMessage id="Article V - No Fiduciary" />
             </h5>
-            <p class="text-muted m-b-20">
+            <p className="text-muted m-b-20">
               <FormattedMessage id="No Member nor SYS token holder shall have fiduciary responsibility to support the value of the SYS token. The Members do not authorize anyone to hold assets, borrow, nor contract on behalf of SYS token holders collectively. This blockchain has no owners, managers or fiduciaries; therefore, no Member shall have beneficial interest in more than 10% of the SYS token supply." />
             </p>
-            <h5 class="m-b-20 b-t-default p-t-20">
+            <h5 className="m-b-20 b-t-default p-t-20">
               <FormattedMessage id="Article VI - Restitution" />
             </h5>
-            <p class="text-muted m-b-20">
+            <p className="text-muted m-b-20">
               <FormattedMessage id="Each Member agrees that penalties for breach of contract may include, but are not limited to, fines, loss of account, and other restitution." />
             </p>
-            <h5 class="m-b-20 b-t-default p-t-20">
+            <h5 className="m-b-20 b-t-default p-t-20">
               <FormattedMessage id="Article VII - Open Source" />
             </h5>
-            <p class="text-muted m-b-20">
+            <p className="text-muted m-b-20">
               <FormattedMessage id="Each Member who makes available a smart contract on this blockchain shall be a Developer. Each Developer shall offer their smart contracts via a free and open source license, and each smart contract shall be documented with a Ricardian Contract stating the intent of all parties and naming the Arbitration Forum that will resolve disputes arising from that contract." />
             </p>
-            <h5 class="m-b-20 b-t-default p-t-20">
+            <h5 className="m-b-20 b-t-default p-t-20">
               <FormattedMessage id="Article VIII - Language" />
             </h5>
-            <p class="text-muted m-b-20">
+            <p className="text-muted m-b-20">
               <FormattedMessage id="Multi-lingual contracts must specify one prevailing language in case of dispute and the author of any translation shall be liable for losses due to their false, misleading, or ambiguous attested translations." />
             </p>
-            <h5 class="m-b-20 b-t-default p-t-20">
+            <h5 className="m-b-20 b-t-default p-t-20">
               <FormattedMessage id="Article IX - Dispute Resolution" />
             </h5>
-            <p class="text-muted m-b-20">
+            <p className="text-muted m-b-20">
               <FormattedMessage id="All disputes arising out of or in connection with this Constitution shall be finally settled under the Rules of Arbitration of the International Chamber of Commerce by one or more arbitrators appointed in accordance with the said Rules." />
             </p>
-            <h5 class="m-b-20 b-t-default p-t-20">
+            <h5 className="m-b-20 b-t-default p-t-20">
               <FormattedMessage id="Article X - Choice of Law" />
             </h5>
-            <p class="text-muted m-b-20">
+            <p className="text-muted m-b-20">
               <FormattedMessage id="Choice of law for disputes shall be, in order of precedence, this Constitution and the Maxims of Equity." />
             </p>
-            <h5 class="m-b-20 b-t-default p-t-20">
+            <h5 className="m-b-20 b-t-default p-t-20">
               <FormattedMessage id="Article XI - Amending" />
             </h5>
-            <p class="text-muted m-b-20">
+            <p className="text-muted m-b-20">
               <FormattedMessage id="This Constitution and its subordinate documents shall not be amended except by a vote of the token holders with no less than 15% vote participation among tokens and no fewer than 10% more Yes than No votes, sustained for 30 continuous days within a 120 day period." />
             </p>
-            <h5 class="m-b-20 b-t-default p-t-20">
+            <h5 className="m-b-20 b-t-default p-t-20">
               <FormattedMessage id="Article XII - Publishing" />
             </h5>
-            <p class="text-muted m-b-20">
+            <p className="text-muted m-b-20">
               <FormattedMessage id="Members may only publish information to the Blockchain that is within their right to publish. Furthermore, Members voluntarily consent for all Members to permanently and irrevocably retain a copy, analyze, and distribute all broadcast transactions and derivative information." />
             </p>
-            <h5 class="m-b-20 b-t-default p-t-20">
+            <h5 className="m-b-20 b-t-default p-t-20">
               <FormattedMessage id="Article XIII - Informed Consent" />
             </h5>
-            <p class="text-muted m-b-20">
+            <p className="text-muted m-b-20">
               <FormattedMessage id="All service providers who produce tools to facilitate the construction and signing of transactions on behalf of other Members shall present to said other Members the full Ricardian contract terms of this Constitution and other referenced contracts. Service providers shall be liable for losses resulting from failure to disclose the full Ricardian contract terms to users." />
             </p>
 
-            <h5 class="m-b-20 b-t-default p-t-20">
+            <h5 className="m-b-20 b-t-default p-t-20">
               <FormattedMessage id="Article XIV - Severability" />
             </h5>
-            <p class="text-muted m-b-20">
+            <p className="text-muted m-b-20">
               <FormattedMessage id="If any part of this Constitution is declared unenforceable or invalid, the remainder will continue to be valid and enforceable." />
             </p>
 
-            <h5 class="m-b-20 b-t-default p-t-20">
+            <h5 className="m-b-20 b-t-default p-t-20">
               <FormattedMessage id="Article XV - Termination of Agreement" />
             </h5>
-            <p class="text-muted m-b-20">
+            <p className="text-muted m-b-20">
               <FormattedMessage id="A Member is automatically released from all revocable obligations under this Constitution 3 years after the last transaction signed by that Member is incorporated into the blockchain. After 3 years of inactivity an account may be put up for auction and the proceeds distributed to all Members according to the system contract provisions then in effect for such redistribution." />
             </p>
 
-            <h5 class="m-b-20 b-t-default p-t-20">
+            <h5 className="m-b-20 b-t-default p-t-20">
               <FormattedMessage id="Article XVI - Developer Liability" />
             </h5>
-            <p class="text-muted m-b-20">
+            <p className="text-muted m-b-20">
               <FormattedMessage id="Members agree to hold software developers harmless for unintentional mistakes made in the expression of contractual intent, whether or not said mistakes were due to actual or perceived negligence." />
             </p>
 
-            <h5 class="m-b-20 b-t-default p-t-20">
+            <h5 className="m-b-20 b-t-default p-t-20">
               <FormattedMessage id="Article XVII - Consideration" />
             </h5>
-            <p class="text-muted m-b-20">
+            <p className="text-muted m-b-20">
               <FormattedMessage id="All rights and obligations under this Constitution are mutual and reciprocal and of equally significant value and cost to all parties." />
             </p>
 
-            <h5 class="m-b-20 b-t-default p-t-20">
+            <h5 className="m-b-20 b-t-default p-t-20">
               <FormattedMessage id="Article XVIII - Acceptance" />
             </h5>
-            <p class="text-muted m-b-20">
+            <p className="text-muted m-b-20">
               <FormattedMessage id="A contract is deemed accepted when a member signs a transaction which incorporates a TAPOS proof of a block whose implied state incorporates an ABI of said contract and said transaction is incorporated into the blockchain." />
             </p>
 
-            <h5 class="m-b-20 b-t-default p-t-20">
+            <h5 className="m-b-20 b-t-default p-t-20">
               <FormattedMessage id="Article XIX - Counterparts" />
             </h5>
-            <p class="text-muted m-b-20">
+            <p className="text-muted m-b-20">
               <FormattedMessage id="This Constitution may be executed in any number of counterparts, each of which when executed and delivered shall constitute a duplicate original, but all counterparts together shall constitute a single agreement." />
             </p>
           </div>
